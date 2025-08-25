1c3e690042efc0bcc9495e084a694286f6b0257b
Leo <leooproencaaa@gmail.com>
2025-08-25 16:59:16 -0300

    Formatando a moeda em Real Brasileiro.

--- scripts.js
+++ scripts.js
@@ -39,7 +39,8 @@ form.onsubmit = () => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol){
     try {
-        description.textContent = `${symbol} 1 = ${price}`
+        // Exibindo a cotação da moeda selecionada.
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
         //Aplica a classe que exibe o footer para mostrar o resultado.
       footer.classList.add("show-result")
@@ -53,3 +54,13 @@ function convertCurrency(amount, price, symbol){
         alert("Não foi possível converter. Tente novamente mais tarde.")
     }
 }
+
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value){
+    
+   return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+    })
+   }
