f0f284916ecd71ead661c4f33af3ca8c57e9fad8
Leo <leooproencaaa@gmail.com>
2025-08-25 16:52:55 -0300

    Exibindo a cotação da moeda selecionada.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ const GBP = 6.08
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 
 // Manipulando o input amount para receber somente números.
@@ -36,5 +38,18 @@ form.onsubmit = () => {
 
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol){
-    console.log(amount, price, symbol)
+    try {
+        description.textContent = `${symbol} 1 = ${price}`
+
+        //Aplica a classe que exibe o footer para mostrar o resultado.
+      footer.classList.add("show-result")
+    }
+    
+    catch (error) {
+        //Remove a classe do footer removendo ele da tela.
+        footer.classList.remove("show-result")
+
+        console.log(error)
+        alert("Não foi possível converter. Tente novamente mais tarde.")
+    }
 }
